42394dc6401dab2f981119a5116230ae12259e97
yestannn <55324492+yestannn@users.noreply.github.com>
2020-06-12 21:34:36 +0600

    Update Add.java

--- Add.java
+++ Add.java
@@ -18,12 +18,9 @@ import mainpackage.Main;
 
 public class Add extends Main{
 	public static void add_contact() throws IOException, FileNotFoundException{
-		File file1 = new File(System.getProperty("user.dir")+"/src/contacts.txt");// we get the file
-		BufferedReader reader1 = new BufferedReader(new FileReader(file1)); // we get reader for the file
-		OutputStreamWriter writer1 = new OutputStreamWriter(
-                new FileOutputStream(System.getProperty("user.dir")+"/src/contacts.txt", true), "UTF-8");
-		BufferedWriter writer = new BufferedWriter(writer1);//this is a way to get a writer for th specific file
-		Scanner input= new Scanner(System.in);
+		Main.read_txt();
+		
+		
 		boolean duplicate, valid;
 		String currentLine1;
 		String f1 = "";//I initialize the variablesto avoid errors
